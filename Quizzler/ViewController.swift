85b391b87dfc3d04b6b6a491dc75c8a8fe6eddc8
Giovanna Rodrigues <giovannamendessr@gmail.com>
2021-09-09 17:30:31 -0300

    checking the answers and changing text label

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Quizzler
 //
-//  Created by Marcelo Rodrigues de Sousa on 09/09/21.
+//  Created by Giovanna Mendes de Sousa Rodrigues on 09/09/21.
 //
 
 import UIKit
@@ -14,13 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [["4 + 3 = 5","False"],["4 + 2 = 6","True"],["70 - 8 = 62","True"]]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateQuestion()
     }
 
     @IBAction func answerPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer =  quiz[questionNumber][1]
+        if(userAnswer == actualAnswer){
+            print("Correct!")
+        }
+        else{print("Wrong!")}
+        let numberOfQuestions = quiz.count - 1
+        if(questionNumber < numberOfQuestions)
+        {
+            questionNumber += 1
+        }
+        else{questionNumber = 0}
+        updateQuestion()
     }
     
+    func updateQuestion(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
